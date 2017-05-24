34919acce2ce09a4507692b50caf1147f9f35a8d
Adole Samuel <adolesamuel4@gmail.com>
2017-05-24 12:51:08 +0100

    finish sign up and sign in

--- server/index.js
+++ server/index.js
@@ -1,115 +1,130 @@
-var express = require("express");
-var app = express();
-var	bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-var	firebase = require("firebase");
+const express = require("express");
+const app = express();
+const	bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const	firebase = require("firebase");
 
-var path = require('path');
-var port = process.env.PORT || 3555;
+let path = require('path');
+let port = process.env.PORT || 3555;
 
 //firebase setup==========================
-var config ={
-	apiKey: "AIzaSyDaZcVz-je_sdDjTLodyhn3ZFYxzAPbpPs",
-    authDomain: "testapp-6318a.firebaseapp.com",
-    databaseURL: "https://testapp-6318a.firebaseio.com",
-    projectId: "testapp-6318a",
-    storageBucket: "testapp-6318a.appspot.com",
-    messagingSenderId: "845061257899"
-};
-firebase.initializeApp(config);
-var db = firebase.database();
-	usersRef = db.ref("Users");
-//==============================================
+
+let config = {
+    apiKey: "AIzaSyCD2kvr4bfNGOfdMuaWGb-i6ZU3qMdIPNo",
+    authDomain: "post-it-app-e9bab.firebaseapp.com",
+    databaseURL: "https://post-it-app-e9bab.firebaseio.com",
+    projectId: "post-it-app-e9bab",
+    storageBucket: "post-it-app-e9bab.appspot.com",
+    messagingSenderId: "689811360579"
+  };
+  firebase.initializeApp(config);
+
+  let db = firebase.database();
+  	usersRef = db.ref('users');
+
+//==========================================================
 
 
-//Body parser to grab requests from signup form post request
+//Body parser to parse requests into req objexts
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(expressValidator());
 
 
 //This gets route for webpage 
-//test of server.===============================
-app.use(function(req,res,next){
+//test of server.===========================================
+
+app.use((req,res,next) => {
 	console.log("%s request recieved", req.method);
 	next()
 });
 
-app.get('/', function(req,res){
+app.get('/', (req,res) => {
 	res.sendFile(path.join( __dirname + '/index.html'));
 	console.log("You have successfully logged in to homepage");
 });
-
-//configure to handle CORS request 
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POSTS');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, \
-		content-type, Authorization');
-	next();
-});
-//===================================================================
-
 
 //validate elements of the data are not empty
 //check if username or password exist in Database
 //create user if no errors exist and push to database.
 //delete user if user chooses to be deleted
 //==============================================================
-var upRoute = express.Router();
 
-upRoute.use( function(req, res, next) {
-	console.log("someone just came to the app");
+let upRoute = express.Router();
+
+upRoute.use( (req, res, next) => {
+	console.log("User on signup page");
 	// this is where we authenticate users
 	next();
 });
 
 upRoute.route('/user/signup')
-	.post( function( req, res){
-	//create user object
-	var user = {};
-		user.email = req.body.mail;
-		user.username =req.body.uName;
-		user.password = req.body.pWord;
-	//validate elements of the signup form are not empty
-	req.checkBody('mail', "Account can't be opened without email").notEmpty()
-	req.checkBody('uName', "Account can't be opened without name").notEmpty()
-	req.checkBody('pWord', "Account can't be opened without password").notEmpty()
-	var errors = req.validationErrors();
-	if(errors){
-		console.log("empty signup fields present in form");
-		res.json(errors);
-	}
-	else{
-	// this should log items to firebase
-	// check for users with same username or same info before creating.
-	console.log(" check has been run");
-	usersRef.push({ email : req.body.mail,
-			username : req.body.uName,
-			password : req.body.pWord,	
-		}, 
-		function(err){
-			if (err){
-				console.log("error is present"+err);
-			}
-			else{
-				res.json({ message: "Users have successfully signed up"});
-			}
+	.post( ( req, res) => {
+		let email = req.body.mail;
+		let	password = req.body.pWord;
+		let username = req.body.uName;
+		firebase.auth().createUserWithEmailAndPassword(email, password)
+	.then((user) => {
+		// add element to database
+		user.displayName = req.body.uName
+		usersRef.push({
+			email : user.email,
+			username :req.body.uName,
+			password: req.body.pWord
 		});
-}//
+		console.log("User has signed up");
+		res.json({ username: req.body.uName, Password: req.body.pWord, Email : req.body.mail});
+	})
+	.catch((error) => {
+		res.json(error);
+	})
 })
 
-	.get(function(req, res) {
-		// Firebase get all users
-		console.log("Fetched list of all users")
-		usersRef.once("value", function(snapshot, prevChildKey) {
-			res.json(snapshot.val());
-		})
-	});
+//===========sign in route created here===========
+
+let inRoute = express.Router();
+ inRoute.use((req, res, next) => {
+ 	console.log("User is on login page");
+ 	next();
+ })
+ inRoute.route('/user/signin')
+ 	.post( (req, res) => {
+ 		let email = req.body.mail;
+ 		let password = req.body.pWord;
+ 		firebase.auth().signInWithEmailAndPassword(email, password)
+ 		.catch((error) =>{
+ 			res.json(error);
+ 		});
+ 		console.log("User"+email+" has signed in");
+ 	});
+
+///=======================Sign out Route=======================
+
+ let outRoute = express.Router();
+ 	outRoute.route('/user/signout')
+ 		.post( (req, res) => {
+ 			firebase.auth().signOut()
+ 			.then(() => {
+ 				console.log('User signed out');
+ 				res.send('user signed out');
+ 			})
+ 			.catch((error) => {
+ 				console.log('Sign out Error')
+ 				res.send('Sign Out Error');
+ 			});
+ 		});
+
+//=======================Group creation route ======================
+ 
+
+
+
 
 
 //register the routes after creation.
 app.use('/', upRoute);
+app.use('/', inRoute);
+app.use('/', outRoute);
+
 
 //This starts the server on port declared as 3555=========================================
 app.listen(port);
